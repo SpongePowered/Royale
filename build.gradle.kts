0a67940e1e8e1dd07870b78ed2a5b9788636d90b
ImMorpheus <ImMorpheus@users.noreply.github.com>
2021-03-26 22:10:37 +0100

    Use spongegradle and bump dependencies
    Add support for reproducible builds
    Bump kyori minimessage to 4.1.0-SNAPSHOT
    Remove plugins.json and use spongegradle
    Bump minecraft version to 1.16.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,12 @@
+import org.spongepowered.gradle.plugin.config.PluginLoaders
+import org.spongepowered.plugin.metadata.PluginDependency
+
 plugins {
-    java
+    `java-library`
+    id("org.spongepowered.gradle.plugin") version "1.0.2"
     id("org.cadixdev.licenser") version "0.5.0"
     id("com.github.johnrengelman.shadow") version "6.1.0"
+
 }
 
 group = project.group
@@ -16,28 +21,55 @@ repositories {
 
 dependencies {
     implementation("org.spongepowered:spongeapi:8.+")
-    implementation("net.kyori:adventure-text-minimessage:4.0.0-SNAPSHOT") {
+    implementation("net.kyori:adventure-text-minimessage:4.1.0-SNAPSHOT") {
         exclude(group = "net.kyori", module = "adventure-api")
     }
-    implementation("commons-io:commons-io:2.5")
 }
 
+sponge {
+    apiVersion("${project.properties["apiVersion"]}")
+    plugin("royale") {
+        loader(PluginLoaders.JAVA_PLAIN)
+        displayName("${project.properties["name"]}")
+        mainClass("org.spongepowered.royale.Royale")
+        description("Battle Royale, now on Sponge!")
+        links {
+            homepage("https://spongepowered.org")
+            source("https://github.com/SpongePowered/Royale")
+            issues("https://github.com/SpongePowered/Royale/issues")
+        }
+        contributor("Spongie") {
+            description("Lead Developer")
+        }
+        dependency("spongeapi") {
+            loadOrder(PluginDependency.LoadOrder.AFTER)
+            optional(false)
+        }
+    }
+}
+
+val javaTarget = 8 // Sponge targets a minimum of Java 8
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.toVersion(javaTarget)
+    targetCompatibility = JavaVersion.toVersion(javaTarget)
+}
+
+tasks.withType(JavaCompile::class).configureEach {
+    options.apply {
+        encoding = "utf-8" // Consistent source file encoding
+        if (JavaVersion.current().isJava10Compatible) {
+            release.set(javaTarget)
+        }
+    }
 }
 
 tasks {
-    compileJava {
-        options.compilerArgs.addAll(listOf("-Xmaxerrs", "1000"))
-        options.encoding = "UTF-8"
-    }
     jar {
         manifest {
             attributes(mapOf(
                     "Implementation-Title" to project.name,
                     "Implementation-Version" to project.version,
-                    "Implementation-Vendor" to "SpongePowered"
+                    "Implementation-Vendor" to project.properties["organization"]
             )
             )
         }
@@ -63,4 +95,11 @@ license {
 
     include("**/*.java")
     newLine = false
+}
+
+
+// Make sure all tasks which produce archives (jar, sources jar, javadoc jar, etc) produce more consistent output
+tasks.withType(AbstractArchiveTask::class).configureEach {
+    isReproducibleFileOrder = true
+    isPreserveFileTimestamps = false
 }
